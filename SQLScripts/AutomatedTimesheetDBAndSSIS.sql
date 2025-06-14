853cfee8cb36e32b0ea0d62a4241af81569d3b90
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-15 01:06:40 +0200

    Adding modified folders and files for Timesheet Project

--- SQLScripts/AutomatedTimesheetDBAndSSIS.sql
+++ SQLScripts/AutomatedTimesheetDBAndSSIS.sql
@@ -344,3 +344,22 @@ REFERENCES [dbo].[Employee] ([EmployeeID])
 GO
 
 
+DECLARE @ProjectBinary VARBINARY(MAX);
+
+-- read the .ispac file as a single BLOB
+SELECT @ProjectBinary = BulkColumn
+FROM   OPENROWSET(
+         BULK '$(IspacFullPath)',         
+         SINGLE_BLOB
+       ) AS ProjectFile;
+
+EXEC SSISDB.catalog.deploy_project
+     @folder_name   = N'Automated_Timesheet_Project',
+     @project_name  = N'SSIS_Automated_Timesheet_Project',
+     @project_stream= @ProjectBinary,
+     @operation_id  = NULL;                
+
+
+EXEC SSISDB.catalog.create_folder 
+     @folder_name = N'AutomatedTimesheetProject',
+     @folder_id   = NULL;
