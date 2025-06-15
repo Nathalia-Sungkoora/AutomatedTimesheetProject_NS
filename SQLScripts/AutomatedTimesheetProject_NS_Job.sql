4e0b8f0c1e099e02bf2bdd3a5059a9cf38eb1d11
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-15 02:19:59 +0200

    Adding more changes to fix Errors in workflow

--- SQLScripts/AutomatedTimesheetProject_NS_Job.sql
+++ SQLScripts/AutomatedTimesheetProject_NS_Job.sql
@@ -1,70 +1,62 @@
-USE [msdb]
+USE msdb;
 GO
 
-/****** Object:  Job [AutomatedTimesheetProject_NS_Job] ******/
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]] ******/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
-BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+DECLARE @jobName NVARCHAR(100) = N'AutomatedTimesheetProject_NS_Job';
+DECLARE @jobId UNIQUEIDENTIFIER;
+DECLARE @shouldReplace BIT = 1; -- Change to 0 if you don't want auto-replacement
+DECLARE @startDate INT = CONVERT(INT, CONVERT(VARCHAR, GETDATE(), 112));  -- Fix here
 
+-- Check if the job exists
+SELECT @jobId = job_id FROM msdb.dbo.sysjobs WHERE name = @jobName;
+
+-- Drop job if exists and replacement is enabled
+IF @jobId IS NOT NULL AND @shouldReplace = 1
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = @jobName;
+    SET @jobId = NULL;
 END
 
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'AutomatedTimesheetProject_NS_Job', 
-		@enabled=1, 
-		@notify_level_eventlog=0, 
-		@notify_level_email=1, 
-		@notify_level_netsend=0, 
-		@notify_level_page=0, 
-		@delete_level=0, 
-		@description=N'No description available.', 
-		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'LAPTOP-JUKB3QVS\NathaliaSungkoora', 
-		@notify_email_operator_name=N'Nathalia Sungkoora', @job_id = @jobId OUTPUT
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [ImportData]    ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'ImportData', 
-		@step_id=1, 
-		@cmdexec_success_code=0, 
-		@on_success_action=1, 
-		@on_success_step_id=0, 
-		@on_fail_action=2, 
-		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
-		@os_run_priority=0, @subsystem=N'SSIS', 
-		@command=N'/ISSERVER "\"\SSISDB\AutomatedTimesheetProject\SSIS_Automated_Timesheet_Project\ImportTimesheetDataFinal.dtsx\"" /SERVER "\"LAPTOP-JUKB3QVS\"" /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
-		@database_name=N'master', 
-		@flags=0, 
-		@proxy_name=N'SSIS_RunAs_WindowsUser'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'RerunPackage', 
-		@enabled=1, 
-		@freq_type=4, 
-		@freq_interval=1, 
-		@freq_subday_type=4, 
-		@freq_subday_interval=1, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=0, 
-		@active_start_date=20250612, 
-		@active_end_date=20250731, 
-		@active_start_time=0, 
-		@active_end_time=235959, 
-		@schedule_uid=N'88229aa4-a663-4ea3-81d5-3cd403c86dd3'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:
+-- Only create if it doesn't exist
+IF @jobId IS NULL
+BEGIN
+    -- Create job
+    EXEC msdb.dbo.sp_add_job 
+        @job_name = @jobName,
+        @enabled = 1,
+        @description = N'Runs the Import Timesheet Data SSIS package.',
+        @owner_login_name = N'LAPTOP-JUKB3QVS\NathaliaSungkoora',
+        @job_id = @jobId OUTPUT;
+
+    -- Add job step
+    EXEC msdb.dbo.sp_add_jobstep 
+        @job_id = @jobId,
+        @step_name = N'ImportTimesheetData',
+        @subsystem = N'SSIS',
+        @command = N'/ISSERVER "\"\SSISDB\AutomatedTimesheetProject\SSIS_Automated_Timesheet_Project\ImportTimesheetDataFinal.dtsx\"" /SERVER "\"LAPTOP-JUKB3QVS\"" /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E',
+        @database_name = N'master',
+        @on_success_action = 1,
+        @on_fail_action = 2,
+		@proxy_name=N'SSIS_RunAs_WindowsUser';
+
+    -- Add schedule
+    EXEC msdb.dbo.sp_add_schedule 
+        @schedule_name = N'Run every minute',
+        @enabled = 1,
+        @freq_type = 4,  -- Daily
+        @freq_interval = 1,
+        @freq_subday_type = 4,  -- Minutes
+        @freq_subday_interval = 1,
+        @active_start_date = @startDate,     -- use the variable here
+        @active_start_time = 0;
+
+    -- Attach schedule
+    EXEC msdb.dbo.sp_attach_schedule 
+        @job_name = @jobName,
+        @schedule_name = N'Run every minute';
+
+    -- Register the job to run on local server
+    EXEC msdb.dbo.sp_add_jobserver 
+        @job_name = @jobName,
+        @server_name = N'(local)';
+END
 GO
-
-
